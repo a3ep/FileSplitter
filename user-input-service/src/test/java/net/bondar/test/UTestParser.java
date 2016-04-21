7a970da38e0e94ea66b5f7ce01529e13cc553e7e
Vsevolod Bondar <bondvsev@gmail.com>
2016-04-21 10:42:55 +0300

    Fixed UTestParser .

--- user-input-service/src/test/java/net/bondar/test/UTestParser.java
+++ user-input-service/src/test/java/net/bondar/test/UTestParser.java
@@ -6,13 +6,9 @@ import net.bondar.splitter.utils.ApplicationParameterHolder;
 import net.bondar.user_input.interfaces.IParameterParser;
 import net.bondar.user_input.utils.CliParameterParser;
 import org.easymock.EasyMock;
-import org.junit.After;
-import org.junit.Test;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.testng.annotations.Test;
 
 import static org.easymock.EasyMock.expect;
 import static org.junit.Assert.assertEquals;
@@ -34,15 +30,16 @@ public class UTestParser {
     private static String partSuffix;
 
     /**
-     * List of correct arguments.
+     * Initializes parser instance.
      */
-    private List<String> correctArgs = new ArrayList<>();
-
-    /**
-     * List of incorrect arguments.
-     */
-    private List<String> incorrectArgs = new ArrayList<>();
-
+    @BeforeClass
+    public static void setUp() {
+        IParameterHolder paramHolder = EasyMock.createMock(ApplicationParameterHolder.class);
+        paramParser = new CliParameterParser(paramHolder);
+        expect(paramHolder.getValue("partSuffix")).andReturn("_part_").times(0, Integer.MAX_VALUE);
+        EasyMock.replay(paramHolder);
+        partSuffix = paramHolder.getValue("partSuffix");
+    }
 
     /**
      * Initializes correct testing arguments.
@@ -67,47 +64,16 @@ public class UTestParser {
     public Object[][] createData2() {
         return new Object[][]{
                 {"split, -p, /test.txt, -s, 5M"},
-                {"merge, -p, /test/test.txt\" + partSuffix + \"001",},
+                {"merge, -p, /test/test.txt" + partSuffix + "001"},
                 {"help"},
                 {"exit"}
         };
     }
 
-
-    /**
-     * Initializes parser instance.
-     */
-    @org.testng.annotations.BeforeClass
-    public static void setUp() {
-        IParameterHolder paramHolder = EasyMock.createMock(ApplicationParameterHolder.class);
-        paramParser = new CliParameterParser(paramHolder);
-        expect(paramHolder.getValue("partSuffix")).andReturn("_part_").times(0, Integer.MAX_VALUE);
-        EasyMock.replay(paramHolder);
-        partSuffix = paramHolder.getValue("partSuffix");
-    }
-
-//    /**
-//     * Initializes testing arguments.
-//     */
-//    @Before
-//    public void setUpTestParseCorrect(){
-//        incorrectArgs.add("abc");
-//        incorrectArgs.add("split, -p, asdffsa");
-//        incorrectArgs.add("split, -p, /home/test/test.txt, -s, asdasdas");
-//        incorrectArgs.add("merge");
-//        incorrectArgs.add("merge, -p, 1231231");
-//        incorrectArgs.add("");
-//        incorrectArgs.add("-p, /home/test/test.test, -s, 5M");
-//        correctArgs.add("split, -p, /test.txt, -s, 5M");
-//        correctArgs.add("merge, -p, /test/test.txt" + partSuffix + "001");
-//        correctArgs.add("help");
-//        correctArgs.add("exit");
-//    }
-
     /**
      * Tests the applications work with incorrect arguments.
      */
-    @org.testng.annotations.Test(dataProvider = "testParseCorrect")
+    @Test(dataProvider = "testParseIncorrect")
     public void testParseIncorrect(String arg) {
         try {
             paramParser.parse(arg.split(", "));
@@ -115,42 +81,13 @@ public class UTestParser {
         } catch (ApplicationException e) {
             assertEquals(ApplicationException.class, e.getClass());
         }
-//        for (String[] arg : getArgs(incorrectArgs)) {
-//            try {
-//                paramParser.parse(arg);
-//                fail("Expected incorrect arguments. Arguments: " + Arrays.toString(arg));
-//            } catch (ApplicationException e) {
-//                assertEquals(ApplicationException.class, e.getClass());
-//            }
-//        }
     }
 
     /**
      * Tests the applications work with correct arguments.
      */
-    @Test
-    public void testCorrectArguments() {
-        for (String[] arg : getArgs(correctArgs)) {
-            paramParser.parse(arg);
-        }
-    }
-
-    /**
-     * Cleaning testing arguments.
-     */
-    @After
-    public void tearDown() {
-        incorrectArgs.clear();
-        correctArgs.clear();
-    }
-
-    /**
-     * Gets list of arguments.
-     *
-     * @param argsToString list of arguments strings
-     * @return list of  arguments arrays
-     */
-    private List<String[]> getArgs(List<String> argsToString) {
-        return argsToString.stream().map(arg -> arg.split(", ")).collect(Collectors.toList());
+    @Test(dataProvider = "testParseCorrect")
+    public void testCorrectArguments(String arg) {
+        paramParser.parse(arg.split(", "));
     }
 }
